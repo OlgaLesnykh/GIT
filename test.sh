0aec4ebc6ca30d107f789daa4e0944e43066bf0e
Olga Lesnykh <korobeynicheva@gmail.com>
2023-06-08 22:27:49 +0300

    Add calculation

--- test.sh
+++ test.sh
@@ -3,3 +3,10 @@ echo "Enter the first number A:"
 read a
 echo "Enter the second number B:"
 read b
+if [[ $a -gt $b ]]; then
+	echo "A - B = $((a-b))";
+elif [[ $a -eq $b ]]; then
+	echo "A^B = $((a**a))";
+else
+	echo "B - A = $((b-a))";
+fi;
